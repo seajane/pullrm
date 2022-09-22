3f3aeae0a9d42bb079da72f90a3ca1d7ec7e9c21
Heather Bouzek <heather.bouzek@gmail.com>
2022-09-22 15:01:52 -0700

    Added specificity subunit search
    In addition to fixing some bugs with the RM gene search, adding in ability to find specificity subunits.

--- pullrms.py
+++ pullrms.py
@@ -14,6 +14,7 @@ import os, re, sys, getopt
 import shutil
 
 
+
 # user functions
 
 ## the csv must have columns named genome_id, genome_name, contig_name, contig_start, contig_end
@@ -154,39 +155,6 @@ except urllib.error.HTTPError as err:
 # 3. From each genbank file, pull CDS
 ## if no CDS - annotate genome
 
-# Extract CDS
-def extract_cds (dfrow, gb, gbk_path, shifted_contig_path):
-    # dfrow = row in the data frame containing contig filenames
-    # gb = 
-    # gbk_path = genebank file path
-    # shifted_contig_path = shifted contig path
-    op = gbk_path + "/" + dfrow['contig_filename']
-    np = shifted_contig_path + "/" + dfrow['contig_filename']
-    new_cds = pd.DataFrame()
-    # setting col width any lower will cause sequence to be truncated
-    pd.set_option("max_colwidth", 9999)
-    cds1 = [feature for feature in gb.features if feature.type == "CDS"]
-    gene1 = [feature for feature in gb.features if feature.type == "gene"]
-    if len(cds1) == 0:
-        shutil.copyfile(op, np)
-    else:
-        for feature in cds1:
-            position = feature.location
-            start = position.start.real
-            stop = position.end.real
-            strand = position.strand.real
-            try:
-                cds = feature.qualifiers["translation"][0]
-            except KeyError:
-                cds = ""
-            try:
-                product = feature.qualifiers['product']
-            except KeyError:
-                product = ""
-            new_val = pd.DataFrame({'description':[dfrow['genome_name']],'gb':[dfrow['genome_id']],'emb':[gb.id], 'start':[start], \
-            'stop':[stop], 'strand':[strand], 'product':[product], 'cds':[cds], 'contig_filename':[dfrow['contig_filename']]})
-            new_cds = pd.concat([new_cds, new_val], axis = 1, ignore_index = True)
-    return new_cds
 
 # find features for genes and cds
 def feature_extract (dfrow, gbk_path, working_path, annotation_folder_path):
@@ -195,11 +163,8 @@ def feature_extract (dfrow, gbk_path, working_path, annotation_folder_path):
     os.chdir(gbk_path)
     np = annotation_folder_path + "/" + dfrow['contig_filename']
     fn = dfrow['contig_filename']
-    fn = fn.item().strip()
     descr = dfrow['genome_name']
-    descr = descr.item().strip()
     gbid = dfrow['genome_id']
-    gbid = gbid.item().strip()
     op = "./" + fn
     new_cds = pd.DataFrame()
     new_gene = pd.DataFrame()
@@ -220,7 +185,6 @@ def feature_extract (dfrow, gbk_path, working_path, annotation_folder_path):
         exit
     else:
         cds1 = [feature for feature in gb.features if feature.type == "CDS"]
-        print(cds1)
         gene1 = [feature for feature in gb.features if feature.type == "gene"]
         if len(cds1) == 0:
             print("No CDS found for %s." % gb.id)
@@ -231,140 +195,221 @@ def feature_extract (dfrow, gbk_path, working_path, annotation_folder_path):
                 start = feature.location.start.real
                 stop = feature.location.end.real
                 seq = gb.seq[start:stop]
-                seq = ''.join(seq)
-                new_val = pd.DataFrame({'start':[start],'seq':[seq]})
+                gene_seq = ''.join(seq)
+                new_val = pd.DataFrame({'start':[start],'seq':[gene_seq]})
                 new_gene = pd.concat([new_gene, new_val], axis = 0, ignore_index = True)
-                print(new_gene)
             for feature in cds1:
                 start = feature.location.start.real
-                print(start)
                 stop = feature.location.end.real
                 strand = feature.location.strand.real
-                try:
-                    cds = feature.qualifiers["translation"][0]
-                except KeyError:
-                    record = ftx[ftx['start'] == start]['seq']
-                    record = Seq(record.item())
-                    if len(record) %3 ==0:
-                        cds = record
-                    elif (len(record)+1) %3 ==0:
-                        cds = record + Seq('N')
-                    else:
-                        cds = record + Seq('NN')
-                    cds = cds.reverse_complement().translate(table='Bacterial', to_stop=True) 
-                    cds = ''.join(cds)
                 product = feature.qualifiers['product'][0]
+                if 'translation' in feature.qualifiers:
+                    cdsb = feature.qualifiers["translation"][0]
+                else:
+                    cdsb = None
+                    if gene1 is not None:
+                        record = Seq(gene_seq)
+                        if len(record) %3 ==0:
+                            cds2 = record
+                        elif (len(record)+1) %3 ==0:
+                            cds2 = record + Seq('N')
+                        else:
+                            cds2 = record + Seq('NN')
+                        if isinstance(cds2, str):
+                            cds2 = Seq(cds2)
+                        cds2 = cds2.reverse_complement().translate(table = 'Bacterial', to_stop=True) 
+                        cdsb = ''.join(cds2)
+                    else: 
+                        cdsb = Seq('NNNN')
                 new_val = pd.DataFrame({\
                                         'description':[descr], \
                                         'gb':[gbid],
                                         'emb':[gb.id], \
                                         'start':[start], 'stop':[stop], \
                                         'strand':[strand], 'product':[product], \
-                                        'cds':[cds],\
-                                        'contig_filename':[fn]
+                                        'AA':[cdsb],'contig_filename':[fn]
                                         })
                 new_cds = pd.concat([new_cds, new_val], axis = 0, ignore_index = True)
-                print(new_cds)
-            
         gene_cds = new_gene.merge(new_cds, on='start', how='left')
         count = len(gene_cds)
         print('%d CDS features collected for %s.' % (count, gb.id))
     os.chdir(working_path)
     return gene_cds
 
-def clean_seq(found_gene_row):
-    seq1 = found_gene_row['cds']
-    regex = re.compile('[^A-Z]')
-    seq1 = regex.sub('', seq1)
-    gb1 = re.sub(r"\d+ ","", found_gene_row['gb'])
-    emb1 = re.sub(r"\d+ ","", found_gene_row['emb'])
-    desc1 = re.sub(r"\d+ ","", found_gene_row['description'])
-    try:
-        prod1 = re.sub(r"\d+ ","", found_gene_row['product'][0])
-    except KeyError:
-        prod1 = ''
-    start1 = found_gene_row['start']
-    stop1 = found_gene_row['stop']
+def clean_seq(gene_cds_row, seq_type):
+    if isinstance(gene_cds_row, pd.DataFrame) or  isinstance(gene_cds_row, pd.Series):
+        if seq_type == "DNA":
+            seq1 = gene_cds_row["seq"]
+        else:
+            seq1 = gene_cds_row["AA"]
+        try:
+            prod1 = gene_cds_row["product"]
+        except KeyError:
+            prod1 = ''
+        desc1 = gene_cds_row['description']
+        gb1 = gene_cds_row['gb']
+        emb1 = gene_cds_row['emb']
+        start1 = gene_cds_row['start']
+        stop1 = gene_cds_row['stop']
+    elif isinstance(gene_cds_row, list):
+        # use numbers
+        if seq_type == "DNA":
+            seq1 = gene_cds_row[1]
+        else:
+            seq1 = gene_cds_row[8]
+        try:
+            prod1 = gene_cds_row[7]
+        except KeyError:
+            prod1 = ''
+        desc1 = gene_cds_row[2]
+        gb1 = gene_cds_row[3]
+        emb1 = gene_cds_row[4]
+        start1 = gene_cds_row[0]
+        stop1 = gene_cds_row[5]
+    elif isinstance(gene_cds_row, tuple):
+        gene_cds_row = pd.Series(gene_cds_row)[1]
+        # use numbers
+        if seq_type == "DNA":
+            seq1 = gene_cds_row.iloc[1]
+        else:
+           seq1 = gene_cds_row.iloc[8]
+        #print(seq1)
+        try:
+           prod1 = gene_cds_row[7]
+        except KeyError:
+           prod1 = ''
+        desc1 = gene_cds_row[2]
+        gb1 = gene_cds_row[3]
+        emb1 = gene_cds_row[4]
+        start1 = gene_cds_row[0]
+        stop1 = gene_cds_row[5]
+    desc1 = re.sub(r"\d+ ","", desc1)
     id1 = desc1.strip().replace(" ", "_")
-    name1 = 'gb|' + gb1.strip() + '|emb|' + emb1.strip()
-    desc2 = prod1 + '_' + str(start1) + '_' + str(stop1)
+    name1 = 'gb|' + gb1 + '|emb|' + emb1
+    desc2 = prod1 + '_' + str(start1) + '_' + str(stop1).replace(" ", "_")
     sr = SeqRecord(Seq(seq1), id = id1, description = desc2, name = name1)
     return sr
-
-# Create a blank fasta file and a blank data frame
-gene_rm_seq = orp + "/" + input_csv[:-4] + "gene_rm.fasta"
-cds_rm_seq = orp + "/" + input_csv[:-4] + "cds_rm.fasta"
-gene_sens_seq = orp + "/" + input_csv[:-4] + "gene_sens.fasta"
-cds_sens_seq = orp + "/" + input_csv[:-4] + "cds_sens.fasta"
-phylo_only = pd.DataFrame()
-noseq = pd.DataFrame()
-with open(cds_rm_seq, "w") as output_handle: # send output to a fasta file
-    for index, row in df2.iterrows():
-        ldf = len(df2)
-        print("RMtase CDS File " + str(index) + " of " + str(ldf))
-        ftx = feature_extract(row, grp, rp, prp)
-        # find RM - reduce list to putative start and stop -/+ window respectively
-        if ftx is None or len(ftx) == 0:
-            print("Cannot find " + row['contig_filename'])
-            # want a more specific error recorded
-            noseq = pd.concat([noseq, row], axis = 1, ignore_index = True)
-        else: 
-           # find RM
-           ftx = ftx.assign(close_start = abs(ftx['start']-int(row['search_start'])))
-           minstart = min(ftx['close_start'])
-           ftx = ftx[ftx['close_start'] == minstart]
-           sr = clean_seq(ftx['cds'])
-           # track rows added to fasta file
-           phylo_only = pd.concat([phylo_only, row], axis = 1, ignore_index = True)
-           SeqIO.write(sr, output_handle, 'fasta')
-           
-with open(gene_rm_seq, "w") as output_handle: # send output to a fasta file
-    for index, row in df2.iterrows():
-        ldf = len(df2)
-        print("RMtase Gene File " + str(index) + " of " + str(ldf))
-        ftx = feature_extract(row, grp, rp, prp)
-        # find RM - reduce list to putative start and stop -/+ window respectively
-        if ftx is None or len(ftx) == 0:
-            print("Cannot find " + row['contig_filename'])
-        else: 
-           # find RM
-           ftx = ftx.assign(close_start = abs(ftx['start']-int(row['search_start'])))
-           minstart = min(ftx['close_start'])
-           ftx = ftx[ftx['close_start'] == minstart]
-           sr = clean_seq(ftx['gene'])
-           # track rows added to fasta file
-           SeqIO.write(sr, output_handle, 'fasta')
-       
-with open(gene_sens_seq, "w") as output_handle: # send output to a fasta file
-    for index, row in df2.iterrows():
-        ldf = len(df2)
-        print("Sensitivity Subunit Gene File " + str(index) + " of " + str(ldf))
-        ftx = feature_extract(row, grp, rp, prp)
-        # find RM - reduce list to putative start and stop -/+ window respectively
-        if ftx is None or len(ftx) == 0:
-            print("Cannot find " + row['contig_filename'])
-        else: 
-           # find Sens
-           ftx = ftx.assign(close_start = abs(ftx['start']-int(row['search_start'])))
-           minstart = min(ftx['close_start'])
-           ftx['closest'] = ftx['close_start'] == minstart
-           
-           sr = clean_seq(ftx['gene'])
-           # track rows added to fasta file
-           SeqIO.write(sr, output_handle, 'fasta')         
-            
-
 
 # 4. Pull Restriction-Methyltransferase using OCTAPUS coordinates
 ## OCTAPUS coords are not always correct, may be shifted
 ## Directionality can be inferred from start/stop
 
+def store_seq(dataframe, seq_type, output_csv, output_fasta, output_fail_csv, \
+              genbank_folder_path = grp, home_path = rp, \
+              annotation_folder_path = prp):
+    #colnms = list(dataframe.columns.values)
+    with open(output_fasta, "w") as output_handle: # send output to a fasta file
+        for index, row in dataframe.iterrows():
+            ldf = len(dataframe)
+            print("Feature File " + str(index + 1) + " of " + str(ldf))
+            ftx = feature_extract(row, genbank_folder_path, home_path, annotation_folder_path)
+            # find RM - reduce list to putative start and stop -/+ window respectively
+            if ftx is None or len(ftx) == 0:
+                print("Storing information for " + row['contig_filename'])
+                # want a more specific error recorded
+                if seq_type == "AA":
+                    output_fail_csv = pd.concat([output_fail_csv, pd.DataFrame(row).T], axis = 0, ignore_index = True)
+            else: 
+                # find RM
+                ftx = ftx.assign(close_start = abs(ftx['start']-int(row['search_start'])))
+                minstart = min(ftx['close_start'])
+                ftx = ftx[ftx['close_start'] == minstart]
+                for row1 in ftx.iterrows():
+                    # clean up an annotate
+                    if seq_type == "DNA":
+                        sr = clean_seq(row1, seq_type = "DNA")
+                    else:
+                        sr = clean_seq(row1, seq_type = "AA")
+                    # track rows added to fasta file
+                    if seq_type == "AA":
+                        output_csv = pd.concat([output_csv, ftx], axis = 0, ignore_index = True)
+                    SeqIO.write(sr, output_handle, 'fasta')
+        #write out csv
+        if seq_type == "AA":
+            nm_pass = annotation_folder_path + "/" + "RM_pass.csv"
+            nm_fail = annotation_folder_path + "/" + "RM_fail.csv"
+            output_csv.to_csv(nm_pass)
+            output_fail_csv.to_csv(nm_fail)
+    return
+
+
+# Create a blank fasta file and a blank data frame
+gene_rm_seq = orp + "/" + input_csv[:-4] + "_gene_rm.fasta"
+cds_rm_seq = orp + "/" + input_csv[:-4] + "_cds_rm.fasta"
+gene_sens_seq = orp + "/" + input_csv[:-4] + "_gene_sens.fasta"
+cds_sens_seq = orp + "/" + input_csv[:-4] + "_cds_sens.fasta"
+phylo_only_aa = pd.DataFrame()
+phylo_only_dna = pd.DataFrame()
+sens_aa = pd.DataFrame()
+sens_dna = pd.DataFrame()
+noseq_aa_rm = pd.DataFrame()
+noseq_dna_rm = pd.DataFrame()
+noseq_aa_sens= pd.DataFrame()
+noseq_dna_sens = pd.DataFrame()
+specificity_lookup = pd.DataFrame()
+
+store_seq(dataframe = df2, seq_type = "DNA", output_csv = phylo_only_dna, output_fail_csv = noseq_dna_rm, output_fasta = gene_rm_seq)
+store_seq(dataframe = df2, seq_type = "AA", output_csv = phylo_only_aa, output_fail_csv = noseq_aa_rm, output_fasta = cds_rm_seq)
+           
 # 5. Pull Specificity subunit using directionality and rmtase coords
 ## Usually within 4-7 base pairs
 ## Remove transposon hits
+        
+def store_sens(dataframe, seq_type, output_csv, output_fasta, output_fail_csv, \
+              genbank_folder_path = grp, home_path = rp, \
+              annotation_folder_path = prp):
+    with open(output_fasta, "w") as output_handle: # send output to a fasta file
+        for index, row in dataframe.iterrows():
+            ldf = len(dataframe)
+            print("Feature File " + str(index + 1) + " of " + str(ldf))
+            ftx = feature_extract(row, genbank_folder_path, home_path, annotation_folder_path)
+            # find RM - reduce list to putative start and stop -/+ window respectively
+            if ftx is None or len(ftx) == 0:
+                print("Storing information for " + row['contig_filename'])
+                # want a more specific error recorded
+                if seq_type == "AA":
+                    output_fail_csv = pd.concat([output_fail_csv, pd.DataFrame(row).T], axis = 0, ignore_index = True)
+            else: 
+                # find Sens
+                lft = len(ftx)
+                ftx = ftx.assign(close_start = abs(ftx['start']-int(row['search_start'])))
+                ftx = ftx.assign(indx = list(range(0,lft)))
+                minstart = min(ftx['close_start'])
+                ftx['closest'] = ftx['close_start'] == minstart
+                f2 = ftx[ftx['close_start'] == minstart]
+                f2sens = int(f2['indx']) + int(f2['strand'])
+                try:
+                    ftx  = ftx[ftx['indx']== f2sens]
+                except:
+                    print("Contig doesn't contain Specificity subunit.")
+                    output_csv = pd.concat([specificity_lookup, ftx], axis = 0, ignore_index = True)
+                    return output_csv
+                # clean up an annotate
+                for row1 in ftx.iterrows():
+                    # clean up an annotate
+                    if seq_type == "DNA":
+                        sr = clean_seq(row1, seq_type = "DNA")
+                    else:
+                        sr = clean_seq(row1, seq_type = "AA")
+                    # track rows added to fasta file
+                    if seq_type == "AA":
+                        output_csv = pd.concat([output_csv, ftx], axis = 0, ignore_index = True)
+                    SeqIO.write(sr, output_handle, 'fasta')
+    #write out csv
+    nm_pass = annotation_folder_path + "/" + "Sens_pass.csv"
+    nm_fail = annotation_folder_path + "/" + "Sens_fail.csv"
+    output_csv.to_csv(nm_pass)
+    output_fail_csv.to_csv(nm_fail)
+
+    
+            
+store_sens(dataframe = df2, seq_type = "DNA", output_csv = sens_dna, output_fail_csv = noseq_dna_sens, output_fasta = gene_sens_seq)
+store_sens(dataframe = df2, seq_type = "AA", output_csv = sens_aa, output_fail_csv = noseq_aa_sens, output_fasta = cds_sens_seq)
+
 
 # 6. Create a FASTA file of DNA and AA for results
 ## One for RMtase and one for Specificity
 
 #7. retain name, taxonomy, length of sequence, and accession in a csv file
 ## Can check for outliers
+
